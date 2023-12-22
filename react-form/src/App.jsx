8e156e5d890e5b3ec7c32e8f3684e0ce8e5733ad
Yaqi Fang <cfang322@gmail.com>
2023-12-22 16:12:31 -0500

    Complete form

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,5 +1,13 @@
+// import { createBrowserRouter, routerProvider, outlet } from ' react-router-dom';
+import Form from "./form";
+
 function App() {
-  return <h1> Hello from App </h1>;
+  return (
+    <div classname="App">
+      <h1>Sign Up</h1>
+      <Form />
+    </div>
+  ); 
 }
 
 export default App;
--- react-form/src/Form.jsx
+++ react-form/src/Form.jsx
@@ -1,109 +1,211 @@
-import { useState } from 'react';
-
-// function Form() {
-//     const [firstName, setFirstName] = useState("");
-//     const [email, setEmail] = useState("");
-//     const [phoneNumber, setPhoneNumber] = useState("");
-//     const [phoneType, setPhoneType] = useState("Home");
-//     const [staff, setStaff] = useState("Instructor");
-//     const [bio, setBio] = useState("");
-//     const signUp = false;
-// 
-//     return (
-//         <div>Form</div>
-//     )
-// }
+import { useState } from "react";
+import "./form.css";
 
 function Form() {
-    const [form, setForm] = useState({
-        name: "",
-        email: "",
-        phoneNumber: "",
-        phoneType: "Home",
-        staff: "",
-        bio: "",
-        emailNotifications: false
-    });
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    phoneType: "Home",
+    staff: "Instructor",
+    bio: "",
+    emailNotifications: false,
+  });
 
-    const [errorMessages, setErrorMessages] = useState([]);
-    const validate = () => {
-        let errors = [];
+  const [errorMessages, seterrorMessages] = useState([]);
 
-        if (form.name.length === 0) {
-            errors.push("name can't be blank")
-        };
-        if (form.email.length === 0) {
-            errors.push("email can't be blank")
-        } else if (!form.email.includes("@")) {
-            errors.push("email should have '@' symbol")
-        };
-        if (form.phoneNumber && form.phoneType === undefined) {
-            errors.push("phone type must be selected")
-        };
-        if (form.bio.length > 280) {
-            errors.push("bio should have a character limit of 280")
-        };
+  const validate = () => {
+    let errors = [];
 
-        return errors;
-    };
+    if (form.name.length === 0) {
+      errors.push("name can't be blank");
+    }
 
-    const handleChange = (field) => {
-        return(e) => {
-            switch(field){
-                case "name":
-                    setForm.name(e.target.value)
-                    break;
-                case "email":
-                    setForm.email(e.target.value)
-                break;
-                case "phoneNumber":
-                    setForm.phoneNumber(e.target.value)
-                break;
-                case "phoneType":
-                    setForm.phoneType(e.target.value)
-                break;
-                case "staff":
-                    setForm.staff(e.target.value)
-                break;
-                case "bio":
-                    setForm.bio(e.target.value)
-                break;
-                case "emailNotification":
-                    setForm.emailNotifications(e.target.value)
-                break; 
+    if (form.email.length === 0) {
+      errors.push("email can't be blank");
+    } else if (!form.email.includes("@")) {
+      errors.push("email should have @ symbol");
+    }
 
-                default:
-                    break;
-            }
-        }
-    };
+    if (form.phoneNumber && form.phoneType === undefined) {
+      errors.push("phone type must be selected");
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    if (form.bio.length > 280) {
+      errors.push("bio should have a character limit of 280 characters");
+    }
 
-        let errors = validate(); 
+    return errors;
+  };
 
-        if (errors.length) {
-            setErrorMessages(errors);
-        } else {
-            let user = {
-                form.name,
-                form.email,
-                form.phoneNumber,
-                form.phoneType,
-                form.staff,
-                form.bio,
-                form.emailNotification
-            }
-        }
-    }; 
+  // const handleChange = (field) => {
 
-    return (
+  //   return (e) => {
+  //     switch (field) {
+  //       case "name":
+  //         setForm.name(e.target.value);
+  //         break;
+  //       case "email":
+  //         setForm.email(e.target.value);
+  //         break;
+  //       case "phoneNumber":
+  //         setForm.phoneNumber(e.target.value);
+  //         break;
+  //       case "phoneType":
+  //         setForm.phoneType(e.target.value);
+  //         break;
+  //       case "staff":
+  //         setForm.staff(e.target.value);
+  //         break;
+  //       case "bio":
+  //         setForm.bio(e.target.value);
+  //         break;
+  //       case "emailNotifications":
+  //         setForm.emailNotifications(e.target.value);
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   };
+  // };
 
-    );
-};
+  const handleChange = (field) => (e) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      [field]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+
+    let errors = validate();
+
+    if (errors.length) {
+      seterrorMessages(errors);
+    } else {
+      let user = {
+        ...form,
+      };
+
+      console.log("form submitted", user);
+
+      seterrorMessages([]);
+    }
+  };
+
+  const showErrors = () => {
+    if (!errorMessages.length) {
+      return null;
+    } else {
+      return (
+        <ul>
+          {errorMessages.map((errors, idx) => {
+            return <li key={idx}>{errors}</li>;
+          })}
+        </ul>
+      );
+    }
+  };
+
+  return (
+    <>
+      {showErrors()}
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            value={form.name}
+            onChange={handleChange("name")}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            value={form.email}
+            onChange={handleChange("email")}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            type="text"
+            value={form.phoneNumber}
+            onChange={handleChange("phoneNumber")}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="phoneType">Phone Type:</label>
+          <select
+            id="phoneType"
+            name="phoneType"
+            value={form.phoneType}
+            onChange={handleChange("phoneType")}
+          >
+            <option value="Home">Home</option>
+            <option value="Work">Work</option>
+            <option value="Mobile">Mobile</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="bio">Bio:</label>
+          <input
+            type="text"
+            value={form.bio}
+            placeholder="bio"
+            onChange={handleChange("bio")}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="staff">
+            Instructor:
+            <input
+              type="radio"
+              name="staff"
+              value="Instructor"
+              checked={form.staff === "Instructor"}
+              onChange={handleChange("staff")}
+            />
+          </label>
+
+          <label htmlFor="staff">
+            Student:
+            <input
+              type="radio"
+              name="staff"
+              value="Student"
+              checked={form.staff === "Student"}
+              onChange={handleChange("staff")}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="emailNotification">
+            Sign up for notification:
+            <input
+              type="checkbox"
+              name="emailNotification"
+              checked={form.emailNotifications}
+              onChange={handleChange("emailNotifications")}
+            />
+          </label>
+        </div>
+
+        <div>
+          <input type="submit" value="Sign Up" />
+        </div>
+      </form>
+    </>
+  );
+}
 
 export default Form;
-
-
-
